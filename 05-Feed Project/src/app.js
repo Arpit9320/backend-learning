8f952e874d5f730de8012408245e414b4899733e
Hritik Srivastava <srivastavaarpit1105@gmail.com>
2026-08-10 14:49:01 +0530

    Create CSP service for the Project

--- 05-Feed Project/src/app.js
+++ 05-Feed Project/src/app.js
@@ -1,16 +1,23 @@
 const express = require("express")
 const postModel = require("./models/post.model")
+const multer = require("multer")
+const uploadFile = require("./services/storage.service")
 
 
 const app = express()
 app.use(express.json())
 
-app.post("/create-post", async (req, res)=>{
+const upload = multer({storage: multer.memoryStorage()}) // to store the uploaded file in memory, which memory:- in this case RAM of the server, instead of saving it to disk
+
+app.post("/create-post", upload.single("image"), async (req, res)=>{
 
     console.log(req.body)
+    console.log(req.file)
+
+    const result = await uploadFile(req.file.buffer)
+    console.log(result)
 
 })
-
 
 
 
