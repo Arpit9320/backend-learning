e384bee8a6c410b1b440112b12a36d27cde26994
Hritik Srivastava <srivastavaarpit1105@gmail.com>
2026-08-16 03:34:46 +0530

    Validation and Authentication Implement

--- 06-Authentication/src/app.js
+++ 06-Authentication/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRoutes = require("./routes/auth.routes")
 const cookieParser = require("cookie-parser")
+const postRoutes = require("./routes/post.routes")
 
 
 const app = express();
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+
+app.use("/api/post", postRoutes)
 
 
 
